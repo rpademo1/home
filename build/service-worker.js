23ec2f5e6bf83ae0c2c8704ed79dd2781fffb910
Thang Ho <nhokly2607@gmail.com>
2019-08-08 11:52:39 +0700

    abcd

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rpaweb/precache-manifest.1fd606e0902f62acf6ec8170952d0c95.js"
+  "/home/precache-manifest.685fae5c10b48d8a8a708ed406ffb194.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rpaweb/index.html", {
+workbox.routing.registerNavigationRoute("/home/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import WelcomePage from './pages/WelcomePage';
-import PricePage from './pages/PricePage';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path='/rpaweb' component={DashboardPage} />
+        <Route path='/home' component={DashboardPage} />
         <Route path='/welcome' component={WelcomePage} />
-        <Route path='/price' component={PricePage} />
       </Switch>
     );
   }
